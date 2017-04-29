e655f5f84afad6a38569ed926c2f707cda3ba336
Marco Grier <mlgrier@gmail.com>
2017-04-28 20:02:42 -0500

    Added more JSON code

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -17,7 +17,42 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //this uses Swifty JSON
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = URL(string: urlString) {
+            
+            if let data = try? Data(contentsOf: url) {
+            
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                
+                    //we're ok to parse
+                    
+                    parse(json: json)
+                }
+            }
+        }
+        
+    }
+    
+    func parse(json: JSON) {
+    
+        for result in json["results"].arrayValue {
+        
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title" : title, "body" : body, "sigs" : sigs]
+            
+            petitions.append(obj)
+        }
+        
+        tableView.reloadData()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
